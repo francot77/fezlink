72d8a4c383e5b0cb2777aeb4548d52d49296af6e
francot77 <francotreboux@gmail.com>
2025-05-15 16:42:06 -0300

    fix delete link endpoint

--- src/app/api/links/[id]/route.ts
+++ src/app/api/links/[id]/route.ts
@@ -1,15 +1,8 @@
-// app/api/links/[id]/route.js
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/mongodb';
 import { Link } from '@/app/models/links';
 
-interface DeleteRequestParams {
-    params: {
-        id: string;
-    };
-}
-
-export async function DELETE(req: Request, { params }: DeleteRequestParams) {
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
     await dbConnect();
     const { id } = params;
     await Link.findByIdAndDelete(id);
